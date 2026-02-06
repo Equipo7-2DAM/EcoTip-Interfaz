3dd1d0c56b4f66051b38030b6dd751e08e34c074
miguel <a28715@svalero.com>
2026-02-07 00:47:29 +0100

    Estructura de carpetas del proyecto

---
+++ EcoTipInterface/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react'
+import { Route, Router, BrowserRouter } from 'react-router'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
